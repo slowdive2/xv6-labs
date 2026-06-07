8b1ff8b84fd4ee527f82475c4cea877da2628615
shells <example@example.com>
2026-06-06 23:43:51 -0700

    distribute the initial 8 freelists evenly

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -8,6 +8,7 @@
 #include "spinlock.h"
 #include "riscv.h"
 #include "defs.h"
+#include <stdint.h>
 
 void freerange(void *pa_start, void *pa_end);
 
@@ -28,30 +29,66 @@ struct kmem kmem[NCPU];
 void
 kinit()
 {
-  for(int i = 0; i < NCPU; i++)
+  for(int i = 0; i < NCPU; i++){
     initlock(&kmem[i].lock, "kmem");
+  }
 
-  freerange(end, (void*)PHYSTOP);
+    char *start = end;
+    uintptr_t chunk = ((uintptr_t)PHYSTOP - (uintptr_t)end) / NCPU;
 
+    for(int i = 0; i < NCPU; i++) {
+        char *rng_start = start + i * chunk; // todo : better naming convention
+        char *rng_end = (i == NCPU - 1)
+               ? (char*)PHYSTOP
+               : start + (i + 1) * chunk;
+
+        freerange(rng_start, rng_end, i);
+    }
   // initialize 8 free lists
 }
 
 void
-freerange(void *pa_start, void *pa_end)
+freerange(void *pa_start, void *pa_end, int id)
 {
-  // TODO : give all free mem to cpu
-  // Hypothesis : kfree() is called within context of CPU, therefore we don't need to touch freerange()
-  
-  char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+
+  if(id == -1){ // default behavior
+    char *p;
+    p = (char*)PGROUNDUP((uint64)pa_start);
+    for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+      kfree(p);
+  }
+  else{ // free w/ fixed cpuid
+    char *p;
+    p = (char*)PGROUNDUP((uint64)pa_start);
+    for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+      cpu_kfree(p, id);
+  }
 }
 
 // Free the page of physical memory pointed at by pa,
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
+
+void
+cpu_kfree(void *pa, int id)
+{
+
+  struct run *r;
+
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    panic("kfree");
+
+  // Fill with junk to catch dangling refs.
+  memset(pa, 1, PGSIZE);
+
+  r = (struct run*)pa;
+  acquire(&kmem[id].lock);
+  r->next = kmem[id].freelist;
+  kmem[id].freelist = r;
+  release(&kmem[id].lock);
+}
+
 void
 kfree(void *pa)
 {
